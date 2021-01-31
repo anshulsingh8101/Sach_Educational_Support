47fc1cb07b295865d9297ba9838501f3a1304252
Anshul Singh <61660509+anshulsingh8101@users.noreply.github.com>
2021-01-31 16:47:34 -0300

    Add files via upload

---
+++ CGI.py
@@ -0,0 +1,10 @@
+#!C:\Python\Python391\python.exe
+
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello, My name is ANSHUL </h2')
+print ('</body>')
+print ('</html>')
